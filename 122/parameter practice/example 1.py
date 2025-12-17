27896c7dc3295a479739dad22ddfea6acada78f7
Morgan C <morgan.cheng@vtsd.com>
2025-12-17 13:08:53 -0500

    New File update

---
+++ 122/parameter practice/example 1.py
@@ -0,0 +1,11 @@
+# Method to add numbers
+
+#Write a method that takes in 3 numbers
+# add the first two, and multiply the third
+
+def add_morenumbers(one, two, three):
+    sum = one + two
+    product = sum * three
+    return product
+
+print(add_morenumbers(6,1,7))
